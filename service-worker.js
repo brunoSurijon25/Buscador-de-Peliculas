b2e53b7d332af00316876618ed35813247b0be1d
brunoSurijon25 <154550249+brunoSurijon25@users.noreply.github.com>
2024-07-04 15:55:48 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,17 @@
 const CACHE_NAME = 'cache-v1';
 const CACHE_FILES = [
   '/',
-  '/index.html',
-  '/favoritos.html',
-  '/estilos/main.css',
-  '/app.js/main.js',
-  '/favoritos.js/main.js',
+  'index.html',
+  'favoritos.html',
+  'estilos.css',
+  'app.js',
+  'favoritos.js',
   'libs/pouchdb-8.0.1.min.js',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css',
   'https://cdn.jsdelivr.net/npm/sweetalert2@11',
-  '/img/icono-cine.png',
-  '/img/icono-error.png'
+  'img/icono-cine.png',
+  'img/icono-error.png'
 ];
 
 self.addEventListener('install', function(event) {
